78bb4d2443b0f397eac8830b4f4de685e40771d1
Scippo97 <lorenzo.dimauro97@gmail.com>
2020-08-17 21:47:35 +0200

    Adding first snippets of the LeaderBoard system

---
+++ Assets/Scripts/LeaderBoard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaderBoard
+{
+    public Player Player {get; }
+    public int KillCount { get; }
+
+    public LeaderBoard(Player player, int killCount)
+    {
+        Player = player;
+        KillCount = killCount;
+    }
+}
---
+++ Assets/Scripts/NetworkLeaderboard.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class NetworkLeaderboard : MonoBehaviour
+{
+    private NetworkManager _networkManager;
+    public List<LeaderBoard> leaderBoard;
+
+    private void Start()
+    {
+        _networkManager = GetComponent<NetworkManager>();
+        leaderBoard = new List<LeaderBoard>();
+    }
+
+    public void AddPlayer(Player player)
+    {
+        leaderBoard.Add(new LeaderBoard(player, 0));
+    }
+
+    public void RemovePlayer(Player player)
+    {
+        leaderBoard.RemoveAll(x => x.Player.GetPeer() == player.GetPeer());
+    }
+
+    public void Clear() => leaderBoard.Clear();
+
+    public void Send()
+    {
+        
+    }
+
+    public void SendFinalResult()
+    {
+        var eteroKillCount = 0;
+        var rgbKillCount = 0;
+
+        foreach (var p in leaderBoard)
+        {
+            if (p.Player.Team.Contains("etero")) eteroKillCount++;
+            else rgbKillCount++;
+        }
+
+        int winningTeam;
+
+        if (eteroKillCount > rgbKillCount) winningTeam = 1;
+        else if (rgbKillCount > eteroKillCount) winningTeam = 2;
+        else winningTeam = 3;
+        
+        _networkManager.SendMessageToClient(
+            $"MatchResult@{eteroKillCount}@{rgbKillCount}@{winningTeam}");
+
+    }
+}
