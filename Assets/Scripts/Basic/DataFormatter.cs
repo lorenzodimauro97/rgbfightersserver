1c1c72dc4e154b8a6f3d3cc2ea2645bccc4bf147
Scippo97 <lorenzo.dimauro97@gmail.com>
2020-09-11 15:24:43 +0200

    Brutal removal of all classes, starting back using Enet and MessagePack

---
+++ Assets/Scripts/Basic/DataFormatter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+
+public static class DataFormatter
+{
+    public static string[] StringToArray(string data, string separator)
+    {
+        return Regex.Split(data, separator);
+    }
+
+    public static string CalculateFileHash(string filename)
+    {
+        using (var md5 = MD5.Create())
+        {
+            using (var stream = File.OpenRead(filename))
+            {
+                var hash = md5.ComputeHash(stream);
+                return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+            }
+        }
+    }
+}
